06ca3bcb9760768d5fde795fc514acba41959174
salvadorperezm <salvadorperezm115@gmail.com>
2023-05-29 10:14:04 -0600

    added materials actions

--- src/materials/materials.module.ts
+++ src/materials/materials.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MaterialsService } from './materials.service';
 import { MaterialsController } from './materials.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Material } from './entities/material.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Material])],
   controllers: [MaterialsController],
-  providers: [MaterialsService]
+  providers: [MaterialsService],
 })
 export class MaterialsModule {}
